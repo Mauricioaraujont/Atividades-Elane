3f5fcde45d41475b4c8fcc0f17e5c323ffc29d10
Mauricio Neto <mauricio456araujo@gmail.com>
2025-10-01 22:00:06 -0300

    Add files via upload

---
+++ ATIVIDADE_07.C
@@ -0,0 +1,6 @@
+﻿A construção começou em 21:59...
+1>------ Compilação iniciada: Projeto: Atividade_06, Configuração: Debug x64 ------
+1>Atividade_06.c
+1>Atividade_06.vcxproj -> C:\Users\Maurício\source\repos\Atividade_06\x64\Debug\Atividade_06.exe
+========== Compilação: 1 bem-sucedida, 0 com falha, 0 atualizada, 0 ignorada ==========
+========== Compilação concluído às 21:59 e levou 00,599 segundos ==========
